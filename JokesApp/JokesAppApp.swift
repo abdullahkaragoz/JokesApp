8a09c447582a083d1b32943a7d0d8272f92d96f8
abdullahkaragoz <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>
2024-11-23 14:03:19 +0300

    Initial Commit

---
+++ JokesApp/JokesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppApp.swift
+//  JokesApp
+//
+//  Created by Abdullah Karagöz on 23.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
